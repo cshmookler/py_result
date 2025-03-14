1f665a043c5598204d1216ef21b895204713ecbe
Caden Shmookler <cshmookler@gmail.com>
2025-03-13 21:20:43 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# **py_result**
+
+A compromise between error codes and exceptions.  Provides result types with detailed error messages and stack traces.
+
+This is a port of [cpp_result](https://github.com/cshmookler/cpp_result) for Python.
+
+## Quickstart
+
+#### 1.&nbsp; Install Python and Git.
+
+##### Windows:
+
+2. [Python](https://python.org/downloads/) (interpreted scripting language)
+    - Select the "Add python.exe to PATH" option.
+    - Before closing the setup wizard after setup, click the "Disable path length limit" button.
+1. [Git](https://git-scm.com/downloads/) (distributed version control)
+
+##### MacOS:
+
+```zsh
+brew install python git
+```
+
+##### Linux (Arch):
+
+```bash
+sudo pacman -S python git
+```
+
+#### 2.&nbsp; Clone this project and enter its root directory.
+
+```bash
+git clone https://github.com/cshmookler/py_result
+cd py_result
+```
+
+#### 3.&nbsp; Install this library globally.
+
+```bash
+pip install .
+```
+
+#### 4.&nbsp; (Optional) Run all tests.
+
+```bash
+pytest
+```
---
+++ setup.py
@@ -0,0 +1,42 @@
+import subprocess
+
+from setuptools import setup
+
+
+# The version to use if there aren't any Git tags or Git is not installed.
+placeholder_version: str = "0.0.0"
+
+
+def get_version_from_git() -> str:
+    """Attempts to retrieve the project version from Git.
+
+    Returns:
+        str: The project version from Git or a placeholder version if Git is not installed or there aren't any tags.
+    """
+
+    result = subprocess.run(["git", "describe", "--tags"], capture_output=True)
+
+    if result.returncode != 0:
+        # Failed to get the latest tag from Git.  Use the placeholder version.
+        return placeholder_version
+
+    return result.stdout.decode().partition("-")[0]
+
+
+# https://python-packaging.readthedocs.io/en/latest/
+
+
+setup(
+    name="py_result",
+    url="https://github.com/cshmookler/py_result",
+    version=get_version_from_git(),
+    author="Caden Shmookler",
+    author_email="cshmookler@gmail.com",
+    description="A compromise between error codes and exceptions.",
+    packages=["src.result"],
+    classifiers=[  # The full list of classifiers is at https://pypi.org/classifiers/
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+)
