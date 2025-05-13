5c1b03852ac804fba1fc9077ef27ce956a08aadf
Caden Shmookler <cshmookler@gmail.com>
2025-05-13 00:28:07 -0600

    Fixed a type comparison issue in test_result.
    When comparing with None, do NOT encapsulate the variable in type().
    When comparing to any other type, encapsulate the variable in type().
    Always use the 'is' operator when comparing types.

--- testing/test_result.py
+++ testing/test_result.py
@@ -4,6 +4,6 @@ from result import Error, Result
 
 def test_result_typevar():
     result: Result[None] = None
-    assert type(result) is None
+    assert result is None
     result = Error("error")
     assert type(result) is Error
